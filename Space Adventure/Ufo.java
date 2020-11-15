6d45926c12ae5f5023bb6ee65e52785598409d50
Hawad Ahmad <hawadjan.1@gmail.com>
2020-11-14 20:20:30 -0500

    added animation

--- Space Adventure/Ufo.java
+++ Space Adventure/Ufo.java
@@ -8,6 +8,9 @@ import greenfoot.*;  // (World, Actor, GreenfootImage, Greenfoot and MouseInfo)
  */
 public class Ufo extends Actor
 {
+    GreenfootImage image1 = new GreenfootImage("ufo1.png");
+    GreenfootImage image2 = new GreenfootImage("ufo2.png");
+    int i = 0;
     /**
      * Act - do whatever the Ufo wants to do. This method is called whenever
      * the 'Act' or 'Run' button gets pressed in the environment.
@@ -15,5 +18,16 @@ public class Ufo extends Actor
     public void act() 
     {
         // Add your action code here.
-    }    
+        animate();
+    } 
+    // this function will animate the ufo
+    private void animate() {
+        if (i == 30)
+            setImage(image2);
+        else if (i == 60) {
+            setImage(image1);
+            i = 0;
+        }
+        i++;
+    }
 }
