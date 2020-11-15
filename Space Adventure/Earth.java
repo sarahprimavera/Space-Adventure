091c10b2925c4d3b86c53ab36d8d16694ec07650
Hawad Ahmad <hawadjan.1@gmail.com>
2020-11-14 20:07:45 -0500

    added actors with assets

---
+++ Space Adventure/Earth.java
@@ -0,0 +1,19 @@
+import greenfoot.*;  // (World, Actor, GreenfootImage, Greenfoot and MouseInfo)
+
+/**
+ * Write a description of class Earth here.
+ * 
+ * @author (your name) 
+ * @version (a version number or a date)
+ */
+public class Earth extends Actor
+{
+    /**
+     * Act - do whatever the Earth wants to do. This method is called whenever
+     * the 'Act' or 'Run' button gets pressed in the environment.
+     */
+    public void act() 
+    {
+        // Add your action code here.
+    }    
+}
