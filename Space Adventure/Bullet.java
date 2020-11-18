050822d8c5f3bea8344bb847bf5003ae6ed2ad3d
Hawad Ahmad <hawadjan.1@gmail.com>
2020-11-17 22:45:33 -0500

    Added bullet class

--- Space Adventure/Bullet.java
+++ Space Adventure/Bullet.java
@@ -8,12 +8,16 @@ import greenfoot.*;  // (World, Actor, GreenfootImage, Greenfoot and MouseInfo)
  */
 public class Bullet extends Actor
 {
+    int speed = 10;
+    public Bullet(int rotation){
+        setRotation(rotation);
+    }
     /**
      * Act - do whatever the Bullet wants to do. This method is called whenever
      * the 'Act' or 'Run' button gets pressed in the environment.
      */
     public void act() 
     {
-        // Add your action code here.
+        //move();
     }    
 }
