c082d3ca5e71adf0b6e2f60dc8f36f0becfe6d70
Hawad Ahmad <hawadjan.1@gmail.com>
2020-11-17 22:46:25 -0500

    World adds planet and creates dynamic background

--- Space Adventure/MyWorld.java
+++ Space Adventure/MyWorld.java
@@ -13,10 +13,16 @@ public class MyWorld extends World
      * Constructor for objects of class MyWorld.
      * 
      */
+    private int level = 1;
     public MyWorld()
     {    
-        // Create a new world with 600x400 cells with a cell size of 1x1 pixels.
-        super(600, 400, 1); 
+        // Create a new world with 800x600 cells with a cell size of 1x1 pixels.
+        super(800, 600, 1); 
+        GreenfootImage background = getBackground();
+        background.setColor(Color.BLACK);
+        background.fill();
+        addStars(350);
+        
         prepare();
     }
 
@@ -26,14 +32,39 @@ public class MyWorld extends World
      */
     private void prepare()
     {
+        // add the spaceship in the middle
         Spaceship spaceship = new Spaceship();
-        addObject(spaceship,61,182);
-        spaceship.setLocation(50,372);
-        spaceship.setLocation(294,245);
-        spaceship.setLocation(301,221);
-        Spaceship spaceship2 = new Spaceship();
-        addObject(spaceship2,369,238);
-        removeObject(spaceship);
-        spaceship2.setLocation(298,231);
+        addObject(spaceship, getWidth()/2, getHeight()/2);
+        // add earth in top right corner
+        Earth earth = new Earth();
+        addObject(earth, getWidth(), earth.getImage().getHeight()/2);
+    }
+    
+    /**
+     * Draw a nmuber of stars for the background
+     * Inspired by the asteroids example
+     */
+    private void addStars(int count) {
+        GreenfootImage background = getBackground();
+        for (int i = 0; i < count; i++){
+            int x = Greenfoot.getRandomNumber(getWidth());
+            int y = Greenfoot.getRandomNumber(getHeight());
+            background.setColor(Color.WHITE);
+            background.fillOval(x, y, 2, 2);
+        }
+    }
+    public void nextLevel(){
+        level++;
+        // if we are at level 2, show the ice planet
+        if (level == 2) {
+            Ice ice = new Ice();
+            addObject(ice, 800, ice.getImage().getHeight()/2);
+        }
+        // if we are at level 3, show the lava planet
+        else if (level == 3) {
+            Lava lava = new Lava();
+            addObject(lava, 800, lava.getImage().getHeight()/2);
+        }
+        // if we are at level 4, it means that the game is over
     }
 }
