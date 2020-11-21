bc861b1b754ee634eddd23eb35c3a0e50e730f7e
Hawad Ahmad <hawadjan.1@gmail.com>
2020-11-20 19:06:58 -0500

    Gave movement

--- Space Adventure/Asteroid.java
+++ Space Adventure/Asteroid.java
@@ -6,14 +6,19 @@ import greenfoot.*;  // (World, Actor, GreenfootImage, Greenfoot and MouseInfo)
  * @author (your name) 
  * @version (a version number or a date)
  */
-public class Asteroid extends Actor
+public class Asteroid extends SmoothMover
 {
     /**
      * Act - do whatever the Asteroid wants to do. This method is called whenever
      * the 'Act' or 'Run' button gets pressed in the environment.
      */
+    private int speed = 7;
+    public Asteroid(int rotation) {
+        setRotation(rotation);
+    }
     public void act() 
     {
-        // Add your action code here.
+        move(speed);
+        removeIfAtEdge();
     }    
 }
