30708e201295f1affb3e21b079c6651f2ab8a05f
Hawad Ahmad <hawadjan.1@gmail.com>
2020-11-25 23:08:20 -0500

    Collision detection
    When touches and asteroid, stop the game (for testing). When touches a star, it will reduce the reload speed for 5 seconds

--- Space Adventure/Spaceship.java
+++ Space Adventure/Spaceship.java
@@ -11,6 +11,8 @@ public class Spaceship extends SmoothMover
     int speed = 5;
     int reloadSpeed = 10;
     int reloadCount = reloadSpeed;
+    boolean poweredUp = false;
+    SimpleTimer timer = new SimpleTimer();
     /**
      * Act - do whatever the Spaceship wants to do. This method is called whenever
      * the 'Act' or 'Run' button gets pressed in the environment.
@@ -19,6 +21,12 @@ public class Spaceship extends SmoothMover
     {
        listenKeyboard();
        reloadCount++;
+       if (timer.millisElapsed() > 5000) {
+           reloadSpeed = 10;
+           poweredUp = false;
+       }
+       checkIfTouchingAsteroid();
+       checkIfTouchingStar();
     }  
     /*
      * Move the spaceship by looking at which keys are pressed
@@ -56,6 +64,31 @@ public class Spaceship extends SmoothMover
         if (reloadCount >= reloadSpeed) {
             getWorld().addObject(new Bullet(getRotation()), getX(), getY());
             reloadCount = 0;
+        }
+    }
+    /**
+     * Reduce points if touched an asteroid
+     */
+    private void checkIfTouchingAsteroid() {
+        Asteroid asteroid = (Asteroid) getOneIntersectingObject(Asteroid.class);
+        if (asteroid != null) {
+            // lose points
+            Greenfoot.stop();
+        }
+    }
+    /**
+     * If touched a star, give a faster reload speed
+     */
+    private void checkIfTouchingStar() {
+        Star star = (Star) getOneIntersectingObject(Star.class);
+        if (star != null) {
+            // remove the star
+            getWorld().removeObject(star);
+            // set a shorter reload speed
+            reloadSpeed = 5;
+            poweredUp = true;
+            timer = new SimpleTimer();
+            timer.mark();
         }
     }
 }
