58213bd9cfe00c91d05b84b1da492ab2f4e13b59
Hawad Ahmad <hawadjan.1@gmail.com>
2020-11-25 23:08:39 -0500

    Changed speed
    Changed speed form 10 to 5

--- Space Adventure/Star.java
+++ Space Adventure/Star.java
@@ -12,7 +12,7 @@ public class Star extends SmoothMover
      * Act - do whatever the Star wants to do. This method is called whenever
      * the 'Act' or 'Run' button gets pressed in the environment.
      */
-    private int speed = 10;
+    private int speed = 5;
     public Star(int rotation) {
         setRotation(rotation);
     }
