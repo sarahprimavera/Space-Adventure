e8362f0e03750a7789ccf40c4cfa0f3a8e0f69c6
Hawad Ahmad <hawadjan.1@gmail.com>
2020-11-27 13:58:15 -0500

    Enemy bullet actor

---
+++ Space Adventure/EnemyBullet.java
@@ -0,0 +1,21 @@
+import greenfoot.*;  // (World, Actor, GreenfootImage, Greenfoot and MouseInfo)
+
+/**
+ * Write a description of class EnemyBullet here.
+ * 
+ * @author (your name) 
+ * @version (a version number or a date)
+ */
+public class EnemyBullet extends SmoothMover
+{
+    private int speed = 5;
+    /**
+     * Act - do whatever the EnemyBullet wants to do. This method is called whenever
+     * the 'Act' or 'Run' button gets pressed in the environment.
+     */
+    public void act() 
+    {
+        move(speed);
+        removeIfAtEdge();
+    }    
+}
